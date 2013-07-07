11aa16126f4d939bb217714c6e88ba25a08b4f8a
Xavier Shay <xavier@rhnh.net>
2013-07-07 14:23:36 -0700

    Move `controller_reference` and `controller_class_names` to protected scope so that they are available to subclasses.

--- actionpack/lib/action_dispatch/routing/route_set.rb
+++ actionpack/lib/action_dispatch/routing/route_set.rb
@@ -62,12 +62,16 @@ module ActionDispatch
           raise ActionController::RoutingError, e.message, e.backtrace if default_controller
         end
 
-      private
+      protected
+
+        attr_reader :controller_class_names
 
         def controller_reference(controller_param)
-          const_name = @controller_class_names[controller_param] ||= "#{controller_param.camelize}Controller"
+          const_name = controller_class_names[controller_param] ||= "#{controller_param.camelize}Controller"
           ActiveSupport::Dependencies.constantize(const_name)
         end
+
+      private
 
         def dispatch(controller, action, env)
           controller.action(action).call(env)

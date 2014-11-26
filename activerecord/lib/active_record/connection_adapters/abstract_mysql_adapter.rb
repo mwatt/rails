8d2a5a363702cd332728bc74fe8b8590a43aabcb
Ryuta Kamizono <kamipo@gmail.com>
2014-11-26 18:54:07 +0900

    Add `:unsigned` option support for MySQL numeric data types
    Example:
    
        create_table :foos, id: :primary_key, unsigned: true do |t|
          t.integer :unsigned_integer, unsigned: true
          t.bigint  :unsigned_bigint,  unsigned: true
          t.float   :unsigned_float,   unsigned: true
          t.decimal :unsigned_decimal, unsigned: true, precision: 10, scale: 2
        end

--- activerecord/lib/active_record/connection_adapters/abstract_mysql_adapter.rb
+++ activerecord/lib/active_record/connection_adapters/abstract_mysql_adapter.rb
@@ -14,7 +14,7 @@ module ActiveRecord
       end
 
       class ColumnDefinition < ActiveRecord::ConnectionAdapters::ColumnDefinition
-        attr_accessor :charset
+        attr_accessor :charset, :unsigned
       end
 
       class TableDefinition < ActiveRecord::ConnectionAdapters::TableDefinition
@@ -27,6 +27,7 @@ module ActiveRecord
             column.type = :integer
             column.auto_increment = true
           end
+          column.unsigned ||= options[:unsigned]
           column.charset = options[:charset]
           column
         end
@@ -60,6 +61,11 @@ module ActiveRecord
           create_sql << "#{o.options}"
           create_sql << " AS #{@conn.to_sql(o.as)}" if o.as
           create_sql
+        end
+
+        def visit_ColumnDefinition(o)
+          o.sql_type = type_to_sql(o.type, o.limit, o.precision, o.scale, o.unsigned)
+          super
         end
 
         def visit_AddColumnDefinition(o)
@@ -114,12 +120,23 @@ module ActiveRecord
         spec = {}
         if column.auto_increment?
           spec[:id] = ':bigint' if column.bigint?
+          spec[:unsigned] = 'true' if column.unsigned?
           return if spec.empty?
         else
           spec[:id] = column.type.inspect
           spec.merge!(prepare_column_options(column).delete_if { |key, _| [:name, :type, :null].include?(key) })
         end
         spec
+      end
+
+      def prepare_column_options(column)
+        spec = super
+        spec[:unsigned] = 'true' if column.unsigned?
+        spec
+      end
+
+      def migration_keys
+        super + [:unsigned]
       end
 
       private
@@ -166,6 +183,10 @@ module ActiveRecord
 
         def blob_or_text_column?
           sql_type =~ /blob/i || type == :text
+        end
+
+        def unsigned?
+          /unsigned/ === sql_type
         end
 
         def case_sensitive?
@@ -698,8 +719,8 @@ module ActiveRecord
       end
 
       # Maps logical Rails types to MySQL-specific data types.
-      def type_to_sql(type, limit = nil, precision = nil, scale = nil)
-        case type.to_s
+      def type_to_sql(type, limit = nil, precision = nil, scale = nil, unsigned = nil)
+        sql = case type.to_s
         when 'binary'
           binary_to_sql(limit)
         when 'integer'
@@ -707,8 +728,11 @@ module ActiveRecord
         when 'text'
           text_to_sql(limit)
         else
-          super
+          super(type, limit, precision, scale)
         end
+
+        sql << ' unsigned' if unsigned && type != :primary_key
+        sql
       end
 
       # SHOW VARIABLES LIKE 'name'

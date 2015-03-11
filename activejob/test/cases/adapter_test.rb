a9e9113a57238513a8215f7432f8afbebd6f21b5
Tamir Duberstein <tamird@gmail.com>
2015-03-11 14:57:13 -0700

    `ActiveJob::QueueAdapters::*` are no longer singletons

--- activejob/test/cases/adapter_test.rb
+++ activejob/test/cases/adapter_test.rb
@@ -2,6 +2,11 @@ require 'helper'
 
 class AdapterTest < ActiveSupport::TestCase
   test "should load #{ENV['AJ_ADAPTER']} adapter" do
-    assert_equal "active_job/queue_adapters/#{ENV['AJ_ADAPTER']}_adapter".classify, ActiveJob::Base.queue_adapter.name
+    assert_equal "active_job/queue_adapters/#{ENV['AJ_ADAPTER']}_adapter".classify, ActiveJob::Base.queue_adapter.class.name
+  end
+
+  test 'should forbid nonsense arguments' do
+    assert_raises(ArgumentError) { ActiveJob::Base.queue_adapter = Mutex }
+    assert_raises(ArgumentError) { ActiveJob::Base.queue_adapter = Mutex.new }
   end
 end

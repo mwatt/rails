e69b5d3c3468e52a54ba2b9e58e2d53ad1ca35fa
Kir Shatrov <shatrov@me.com>
2015-03-19 23:57:49 +0200

    WIP

--- activesupport/lib/active_support/core_ext/marshal.rb
+++ activesupport/lib/active_support/core_ext/marshal.rb
@@ -1,21 +1,17 @@
 module MarshalWithAutoloading
-    def load(source)
-      super(source)
-    rescue ArgumentError, NameError => exc
-      if exc.message.match(%r|undefined class/module (.+)|)
-        # try loading the class/module
-        $1.constantize
-        # if it is a IO we need to go back to read the object
-        source.rewind if source.respond_to?(:rewind)
-        retry
-      else
-        raise exc
-      end
+  def load(source)
+    super(source)
+  rescue ArgumentError, NameError => exc
+    if exc.message.match(%r|undefined class/module (.+)|)
+      # try loading the class/module
+      $1.constantize
+      # if it is a IO we need to go back to read the object
+      source.rewind if source.respond_to?(:rewind)
+      retry
+    else
+      raise exc
     end
-end
-
-Marshal.module_eval do
-  class << self
-    prepend(MarshalWithAutoloading)
   end
 end
+
+Marshal.singleton_class.prepend(MarshalWithAutoloading)

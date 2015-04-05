9fd4127274ddb550bfc6e83f69f24d8e300c8ea5
Stephen Bussey <steve.bussey@salesloft.com>
2015-04-05 11:07:01 -0400

    extract default_head into a method and set the variants properly

--- actionpack/lib/action_controller/metal/implicit_render.rb
+++ actionpack/lib/action_controller/metal/implicit_render.rb
@@ -4,16 +4,22 @@ module ActionController
       ret = super
       return ret if performed?
 
+      lookup_context.variants = request.variant
+
       if template_exists?(method, _prefixes)
         default_render
       else
-        logger.info "No template found for #{self.class.name}#{method}, rendering head :no_content"
-        head :no_content
+        default_head
       end
     end
 
     def default_render(*args)
       render(*args)
+    end
+
+    def default_head(*)
+      logger.info "No template found for #{self.class.name}#{action_name}, rendering head :no_content"
+      head :no_content
     end
 
     def method_for_action(action_name)

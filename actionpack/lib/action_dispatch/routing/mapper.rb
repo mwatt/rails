09cea6e6ba50322772847ea16dbf7650a5598a20
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-05-11 14:03:09 +0530

    Remove overridden root method and move it's implementation in original method
     - The root method is defined and documented in Base module and
       decorated in Resource module.
     - The documentation in Base module actually talks about method
       signature of decorated method from Resource module.
     - Argument handling was moved to decorated method in
       https://github.com/rails/rails/commit/977455cc2efb94f40b4c0d46d1842be198ed7c4c
       to handle options such as :as with directly passed path parameter.
     - To avoid the confusion, removed decorated root method and moved it's
       implementation in Base module's root method.
    
     - References - https://github.com/rails/rails/pull/12208 &
                    https://github.com/rails/rails/pull/12208#issuecomment-24350897.

--- actionpack/lib/action_dispatch/routing/mapper.rb
+++ actionpack/lib/action_dispatch/routing/mapper.rb
@@ -367,8 +367,24 @@ module ActionDispatch
         # You should put the root route at the top of <tt>config/routes.rb</tt>,
         # because this means it will be matched first. As this is the most popular route
         # of most Rails applications, this is beneficial.
-        def root(options = {})
-          match '/', { :as => :root, :via => :get }.merge!(options)
+        def root(path, options={})
+          if path.is_a?(String)
+            options[:to] = path
+          elsif path.is_a?(Hash) and options.empty?
+            options = path
+          else
+            raise ArgumentError, "must be called with a path and/or options"
+          end
+
+          if @scope.resources?
+            with_scope_level(:root) do
+              scope(parent_resource.path) do
+                match '/', { :as => :root, :via => :get }.merge!(options)
+              end
+            end
+          else
+            match '/', { :as => :root, :via => :get }.merge!(options)
+          end
         end
 
         # Matches a url pattern to one or more routes.
@@ -1543,26 +1559,6 @@ module ActionDispatch
           mapping = Mapping.build(@scope, @set, URI.parser.escape(path), as, options)
           app, conditions, requirements, defaults, as, anchor = mapping.to_route
           @set.add_route(app, conditions, requirements, defaults, as, anchor)
-        end
-
-        def root(path, options={})
-          if path.is_a?(String)
-            options[:to] = path
-          elsif path.is_a?(Hash) and options.empty?
-            options = path
-          else
-            raise ArgumentError, "must be called with a path and/or options"
-          end
-
-          if @scope.resources?
-            with_scope_level(:root) do
-              scope(parent_resource.path) do
-                super(options)
-              end
-            end
-          else
-            super(options)
-          end
         end
 
         protected

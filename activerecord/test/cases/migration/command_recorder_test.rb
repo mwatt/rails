4f2b83a5add15e34ded095334cda7980b3fd8ccf
Aster Ryan <astersamazing@gmail.com>
2015-05-20 10:05:51 -0400

    Add an invert method for remove_foreign_key

--- activerecord/test/cases/migration/command_recorder_test.rb
+++ activerecord/test/cases/migration/command_recorder_test.rb
@@ -281,9 +281,19 @@ module ActiveRecord
         assert_equal [:remove_foreign_key, [:dogs, :people]], enable
       end
 
+      def test_invert_remove_foreign_key
+        enable = @recorder.inverse_of :remove_foreign_key, [:dogs, :people]
+        assert_equal [:add_foreign_key, [:dogs, :people]], enable
+      end
+
       def test_invert_add_foreign_key_with_column
         enable = @recorder.inverse_of :add_foreign_key, [:dogs, :people, column: "owner_id"]
         assert_equal [:remove_foreign_key, [:dogs, column: "owner_id"]], enable
+      end
+
+      def test_invert_remove_foreign_key_with_column
+        enable = @recorder.inverse_of :remove_foreign_key, [:dogs, :people, column: "owner_id"]
+        assert_equal [:add_foreign_key, [:dogs, :people, column: "owner_id"]], enable
       end
 
       def test_invert_add_foreign_key_with_column_and_name
@@ -291,14 +301,9 @@ module ActiveRecord
         assert_equal [:remove_foreign_key, [:dogs, name: "fk"]], enable
       end
 
-      def test_remove_foreign_key_is_irreversible
-        assert_raises ActiveRecord::IrreversibleMigration do
-          @recorder.inverse_of :remove_foreign_key, [:dogs, column: "owner_id"]
-        end
-
-        assert_raises ActiveRecord::IrreversibleMigration do
-          @recorder.inverse_of :remove_foreign_key, [:dogs, name: "fk"]
-        end
+      def test_invert_remove_foreign_key_with_column_and_name
+        enable = @recorder.inverse_of :remove_foreign_key, [:dogs, :people, column: "owner_id", name: "fk"]
+        assert_equal [:add_foreign_key, [:dogs, :people, name: "fk"]], enable
       end
     end
   end

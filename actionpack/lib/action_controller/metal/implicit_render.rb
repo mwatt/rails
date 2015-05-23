350884f471f233590f40461a070a6813285fc700
Dave Copeland <davetron5000@gmail.com>
2015-05-23 09:46:13 -0400

    Revert "head no_content when there is no template or action performed"
    This reverts commit 0de4a23d1cfa6ef44a27ac8aca5055487282460e.
    
    Conflicts:
    	actionpack/CHANGELOG.md
    	actionpack/lib/action_controller/metal/implicit_render.rb

--- actionpack/lib/action_controller/metal/implicit_render.rb
+++ actionpack/lib/action_controller/metal/implicit_render.rb
@@ -7,12 +7,7 @@ module ActionController
     end
 
     def default_render(*args)
-      if template_exists?(action_name.to_s, _prefixes, variants: request.variant)
-        render(*args)
-      else
-        logger.info "No template found for #{self.class.name}\##{action_name}, rendering head :no_content" if logger
-        head :no_content
-      end
+      render(*args)
     end
 
     def method_for_action(action_name)

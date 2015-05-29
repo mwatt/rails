8af627e4ac48f242011ccf08de6fb838c9cbce80
Kevin Deisz <kevin.deisz@gmail.com>
2015-05-29 09:41:03 -0400

    Allow Enumerable#pluck to take a splat.
    This allows easier integration with ActiveRecord, such that
    AR#pluck will now use Enumerable#pluck if the relation is loaded,
    without needing to hit the database.

--- activerecord/lib/active_record/relation/calculations.rb
+++ activerecord/lib/active_record/relation/calculations.rb
@@ -153,23 +153,27 @@ module ActiveRecord
     # See also +ids+.
     #
     def pluck(*column_names)
-      column_names.map! do |column_name|
-        if column_name.is_a?(Symbol) && attribute_alias?(column_name)
-          attribute_alias(column_name)
-        else
-          column_name.to_s
-        end
-      end
-
-      if has_include?(column_names.first)
-        construct_relation_for_association_calculations.pluck(*column_names)
+      if loaded?
+        @records.pluck(*column_names)
       else
-        relation = spawn
-        relation.select_values = column_names.map { |cn|
-          columns_hash.key?(cn) ? arel_table[cn] : cn
-        }
-        result = klass.connection.select_all(relation.arel, nil, bound_attributes)
-        result.cast_values(klass.attribute_types)
+        column_names.map! do |column_name|
+          if column_name.is_a?(Symbol) && attribute_alias?(column_name)
+            attribute_alias(column_name)
+          else
+            column_name.to_s
+          end
+        end
+
+        if has_include?(column_names.first)
+          construct_relation_for_association_calculations.pluck(*column_names)
+        else
+          relation = spawn
+          relation.select_values = column_names.map { |cn|
+            columns_hash.key?(cn) ? arel_table[cn] : cn
+          }
+          result = klass.connection.select_all(relation.arel, nil, bound_attributes)
+          result.cast_values(klass.attribute_types)
+        end
       end
     end
 

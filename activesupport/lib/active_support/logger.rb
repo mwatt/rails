7afed078d776f565fafa0f0a6630837782eab157
Carl P. Corliss <rabbitt@gmail.com>
2015-06-09 22:38:53 -0400

    Fixes ActiveSupport::Logger level threadsafety - Uses Thread Local variable (logger_level) to keep track of the log level per Logger Object/Thread - Sets a default logger level on Thread.main the first time a level is set for the object - Fallback to using Thread.main's default_logger_level if no local level is defined, or lastly, the class default - Test cases for thread safety issue added for   1. logger level per logger object   2. logger level for main thread vs current thread   3. logger level for current thread
    more thread safety cleanup

--- activesupport/lib/active_support/logger.rb
+++ activesupport/lib/active_support/logger.rb
@@ -43,7 +43,36 @@ module ActiveSupport
 
     def initialize(*args)
       super
-      @formatter = SimpleFormatter.new
+      @formatter   = SimpleFormatter.new
+    end
+
+    def level
+      # The default log level is set the first time the level is set for this object,
+      # which means that if our current thread doesn't have a local level set, then
+      # we attempt to use the main thread's default level, falling back to the class
+      # default otherwise.
+      Thread.current[local_level_key] || Thread.main[default_level_key] || super
+    end
+
+    def level=(level)
+      # Set system-wide default logger level the first time we set our level
+      # Note: this assumes that the first level we set is the level that what
+      # we expect to use as the default for the life of this object.
+      Thread.main[default_level_key] ||= level
+      Thread.current[local_level_key] = level
+    end
+
+    def add(severity, message = nil, progname = nil, &block)
+      return true if @logdev.nil? || (severity || UNKNOWN) < level
+      super
+    end
+
+    Logger::Severity.constants.each do |severity|
+      class_eval(<<-EOT, __FILE__, __LINE__ + 1)
+        def #{severity.downcase}?                # def debug?
+          Logger::#{severity} >= level           #   DEBUG >= level
+        end                                      # end
+      EOT
     end
 
     # Simple formatter which only displays the message.
@@ -53,5 +82,15 @@ module ActiveSupport
         "#{String === msg ? msg : msg.inspect}\n"
       end
     end
+
+    private
+
+      def default_level_key
+        @default_key ||= "#{self.object_id}_default_logger_level"
+      end
+
+      def local_level_key
+        @level_key ||= "#{self.object_id}_local_logger_level"
+      end
   end
 end

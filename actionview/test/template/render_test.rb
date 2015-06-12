b8b8ff2e56df0ecb2d27cd9987a6db7bcaa40eb7
Roque Pinel <repinel@gmail.com>
2015-06-12 15:30:04 -0400

    Fix cache issue when different partials use the same collection
    It adds the partial `virtual_path` to the caching key: instead of something like `views/david/1` it will be `views/customers/_customer/david/1`.

--- actionview/test/template/render_test.rb
+++ actionview/test/template/render_test.rb
@@ -616,7 +616,7 @@ class CachedCollectionViewRenderTest < CachedViewRenderTest
 
   test "with custom key" do
     customer = Customer.new("david")
-    key = ActionController::Base.new.fragment_cache_key([customer, 'key'])
+    key = ActionController::Base.new.fragment_cache_key(['test/_customer', customer, 'key'])
 
     ActionView::PartialRenderer.collection_cache.write(key, 'Hello')
 
@@ -626,7 +626,7 @@ class CachedCollectionViewRenderTest < CachedViewRenderTest
 
   test "automatic caching with inferred cache name" do
     customer = CachedCustomer.new("david")
-    key = ActionController::Base.new.fragment_cache_key(customer)
+    key = ActionController::Base.new.fragment_cache_key(['test/_cached_customer', customer])
 
     ActionView::PartialRenderer.collection_cache.write(key, 'Cached')
 
@@ -636,7 +636,7 @@ class CachedCollectionViewRenderTest < CachedViewRenderTest
 
   test "automatic caching with as name" do
     customer = CachedCustomer.new("david")
-    key = ActionController::Base.new.fragment_cache_key(customer)
+    key = ActionController::Base.new.fragment_cache_key(['test/_cached_customer_as', customer])
 
     ActionView::PartialRenderer.collection_cache.write(key, 'Cached')
 

5c730d7474ca636ca531866ff3f289b63fa762ce
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-06-17 09:53:38 +0900

    Use `SET CONSTRAINTS` for `disable_referential_integrity` without superuser privileges
    ref: 72c1557254

--- activerecord/lib/active_record/connection_adapters/postgresql/referential_integrity.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/referential_integrity.rb
@@ -8,36 +8,32 @@ module ActiveRecord
 
         def disable_referential_integrity # :nodoc:
           if supports_disable_referential_integrity?
-            original_exception = nil
+            tables_constraints = execute(<<-SQL).values
+              SELECT table_name, constraint_name
+              FROM information_schema.table_constraints
+              WHERE constraint_type = 'FOREIGN KEY'
+              AND is_deferrable = 'NO'
+              AND table_name IN (#{tables.collect { |name| quote(name) }.join(",")})
+            SQL
+
+            execute(
+              tables_constraints.collect { |table, constraint|
+                "ALTER TABLE #{quote_table_name(table)} ALTER CONSTRAINT #{constraint} DEFERRABLE"
+              }.join(";")
+            )
 
             begin
-              transaction(requires_new: true) do
-                execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER ALL" }.join(";"))
+              transaction do
+                execute('SET CONSTRAINTS ALL DEFERRED')
+
+                yield
               end
-            rescue ActiveRecord::ActiveRecordError => e
-              original_exception = e
-            end
-
-            begin
-              yield
-            rescue ActiveRecord::InvalidForeignKey => e
-              warn <<-WARNING
-WARNING: Rails was not able to disable referential integrity.
-
-This is most likely caused due to missing permissions.
-Rails needs superuser privileges to disable referential integrity.
-
-    cause: #{original_exception.try(:message)}
-
-              WARNING
-              raise e
-            end
-
-            begin
-              transaction(requires_new: true) do
-                execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER ALL" }.join(";"))
-              end
-            rescue ActiveRecord::ActiveRecordError
+            ensure
+              execute(
+                tables_constraints.collect { |table, constraint|
+                  "ALTER TABLE #{quote_table_name(table)} ALTER CONSTRAINT #{constraint} NOT DEFERRABLE"
+                }.join(";")
+              )
             end
           else
             yield

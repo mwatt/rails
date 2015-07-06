02129cd8ceb6fa16c0c9423f8ef76cd52cbfce3c
Jorge Bejar <jorge@wyeworks.com>
2015-07-06 14:12:00 -0300

    Use URL path extension as format in bad params exception handling

--- actionpack/lib/action_dispatch/http/parameters.rb
+++ actionpack/lib/action_dispatch/http/parameters.rb
@@ -27,9 +27,9 @@ module ActionDispatch
       # Returns a hash with the \parameters used to form the \path of the request.
       # Returned hash keys are strings:
       #
-      #   {'action' => 'my_action', 'controller' => 'my_controller'}
+      #   {'action' => 'my_action', 'controller' => 'my_controller', format => 'html'}
       def path_parameters
-        @env[PARAMETERS_KEY] ||= {}
+        @env[PARAMETERS_KEY] ||= default_path_parameters
       end
 
     private
@@ -52,6 +52,21 @@ module ActionDispatch
           end
         else
           params
+        end
+      end
+
+      def default_path_parameters
+        if format = format_from_path_extension
+          { 'format' => format }
+        else
+          {}
+        end
+      end
+
+      def format_from_path_extension
+        path = @env['action_dispatch.original_path']
+        if match = path.match(/\.(\w+)$/)
+          match.captures.first
         end
       end
     end

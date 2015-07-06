69f522e9e17b551913f84dee18824d72bb041ff8
Sammy Larbi <sam@codeodor.com>
2015-07-06 14:32:14 -0500

    Define #to_json on Enumerable separately from other modules There was a conflict where, when used with a `Struct` and `JSON.dump` it was converting to a `String` instead of a `Hash` and when using `active_support/json` `JSON.dump` it resulted in `SystemStackError: stack level too deep`
    By moving it out to itself, we can provide the correct result without either problem.
    
    Fixes #20775

--- activesupport/lib/active_support/core_ext/object/json.rb
+++ activesupport/lib/active_support/core_ext/object/json.rb
@@ -26,7 +26,7 @@ require 'active_support/core_ext/module/aliasing'
 # bypassed completely. This means that as_json won't be invoked and the JSON gem will simply
 # ignore any options it does not natively understand. This also means that ::JSON.{generate,dump}
 # should give exactly the same results with or without active support.
-[Object, Array, FalseClass, Float, Hash, Integer, NilClass, String, TrueClass, Enumerable].each do |klass|
+[Object, Array, FalseClass, Float, Hash, Integer, NilClass, String, TrueClass].each do |klass|
   klass.class_eval do
     def to_json_with_active_support_encoder(options = nil) # :nodoc:
       if options.is_a?(::JSON::State)
@@ -40,6 +40,14 @@ require 'active_support/core_ext/module/aliasing'
 
     alias_method_chain :to_json, :active_support_encoder
   end
+end
+
+Enumerable.class_eval do
+  def to_json_with_active_support_encoder(options = nil) # :nodoc:
+    # to_json is being invoked directly, use ActiveSupport's encoder
+    ActiveSupport::JSON.encode(self, options)
+  end
+  alias_method_chain :to_json, :active_support_encoder
 end
 
 class Object

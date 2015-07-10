d609048bcdbb5d0aa6511d922630c384fb838119
Griffin Smith <wildgriffin45@gmail.com>
2015-07-10 11:32:18 -0400

    Support :if and :unless in has_secure_token
    Pass through :if and :unless options from has_secure_token to the
    generated before_create callback

--- activerecord/lib/active_record/secure_token.rb
+++ activerecord/lib/active_record/secure_token.rb
@@ -23,11 +23,14 @@ module ActiveRecord
       # Note that it's still possible to generate a race condition in the database in the same way that
       # <tt>validates_uniqueness_of</tt> can. You're encouraged to add a unique index in the database to deal
       # with this even more unlikely scenario.
-      def has_secure_token(attribute = :token)
+      def has_secure_token(attribute = :token, options = {})
         # Load securerandom only when has_secure_token is used.
         require 'active_support/core_ext/securerandom'
+
+        before_create_options = { if: options[:if], unless: options[:unless] }
+
         define_method("regenerate_#{attribute}") { update! attribute => self.class.generate_unique_secure_token }
-        before_create { self.send("#{attribute}=", self.class.generate_unique_secure_token) unless self.send("#{attribute}?")}
+        before_create(before_create_options) { self.send("#{attribute}=", self.class.generate_unique_secure_token) unless self.send("#{attribute}?")}
       end
 
       def generate_unique_secure_token

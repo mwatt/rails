e1120a74d164c51568c18fdb8bc5d6471b1a1d2b
Alberto F. Capel <afcapel@gmail.com>
2015-07-20 00:52:48 +0100

    Avoid intermediate objects in AR::CollectionCacheKey using connection.select_one.

--- activerecord/lib/active_record/collection_cache_key.rb
+++ activerecord/lib/active_record/collection_cache_key.rb
@@ -7,14 +7,18 @@ module ActiveRecord
       query_signature = Digest::MD5.hexdigest(collection.to_sql)
       key = "#{collection.model_name.cache_key}/query-#{query_signature}"
 
-      size, timestamp = if collection.loaded?
-        [collection.size, collection.collect(&timestamp_column).compact.max]
+      if collection.loaded?
+        size = collection.size
+        timestamp = collection.max_by(&timestamp_column).public_send(timestamp_column)
       else
         column_type = collection.klass.type_for_attribute(timestamp_column.to_s)
         column = "#{connection.quote_table_name(collection.table_name)}.#{connection.quote_column_name(timestamp_column)}"
 
-        result = collection.select("COUNT(*) AS size", "MAX(#{column}) AS timestamp").to_a.first
-        [result.size, column_type.deserialize(result.timestamp)]
+        query = collection.select("COUNT(*) AS size", "MAX(#{column}) AS timestamp")
+        result = connection.select_one(query)
+
+        size = result["size"]
+        timestamp = column_type.deserialize(result["timestamp"])
       end
 
       if timestamp

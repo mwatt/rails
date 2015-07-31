ca072708932077365140668510b96ad3de90fb1c
Mauricio Gomez Aguinaga <mgomezaguinaga@groupon.com>
2015-07-31 14:44:18 -0700

    Parametrize break sequence for word_wrap on ActionView Text Helpers
    Sometimes you need a specific break sequence while using word wrap and as today the only option we have is "\n" and is hardcoded.
    
    With this change you will be able to specify any break sequence ("\r\n" for example) as an option.

--- actionview/lib/action_view/helpers/text_helper.rb
+++ actionview/lib/action_view/helpers/text_helper.rb
@@ -252,10 +252,11 @@ module ActionView
       #   # => Once\nupon\na\ntime
       def word_wrap(text, options = {})
         line_width = options.fetch(:line_width, 80)
+        break_sequence = options.fetch(:break_sequence, "\n")
 
         text.split("\n").collect! do |line|
-          line.length > line_width ? line.gsub(/(.{1,#{line_width}})(\s+|$)/, "\\1\n").strip : line
-        end * "\n"
+          line.length > line_width ? line.gsub(/(.{1,#{line_width}})(\s+|$)/, "\\1#{break_sequence}").strip : line
+        end * break_sequence
       end
 
       # Returns +text+ transformed into HTML using simple formatting rules.

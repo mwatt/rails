8dce5552909e5a3e6dbab49578fa548c631f33cd
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2015-08-07 16:37:59 -0500

    replace each with each_key when only the key is needed
    Using each_key is faster and more intention revealing.
    
    Calculating -------------------------------------
                    each    31.378k i/100ms
                each_key    33.790k i/100ms
    -------------------------------------------------
                    each    450.225k (± 7.0%) i/s -      2.259M
                each_key    494.459k (± 6.3%) i/s -      2.467M
    
    Comparison:
                each_key:   494459.4 i/s
                    each:   450225.1 i/s - 1.10x slower

--- activerecord/lib/active_record/connection_adapters/postgresql/database_statements.rb
+++ activerecord/lib/active_record/connection_adapters/postgresql/database_statements.rb
@@ -125,7 +125,7 @@ module ActiveRecord
             # then strip them off. Indeed it would be prettier to do this in
             # PostgreSQLColumn.string_to_decimal but would break form input
             # fields that call value_before_type_cast.
-            monies.each do |index, _|
+            monies.each_key do |index|
               data = row[index]
               # Because money output is formatted according to the locale, there are two
               # cases to consider (note the decimal separators):

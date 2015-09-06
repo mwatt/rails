02704e37afc520e8f42dacb86c2b61638d4f10ac
Pavel Pravosud <pavel@pravosud.com>
2015-09-05 17:48:15 -0700

    Make ActiveRecordException descendants accept no arguments

--- activerecord/lib/active_record/migration.rb
+++ activerecord/lib/active_record/migration.rb
@@ -84,26 +84,42 @@ module ActiveRecord
   end
 
   class DuplicateMigrationVersionError < MigrationError#:nodoc:
-    def initialize(version)
-      super("Multiple migrations have the version number #{version}")
+    def initialize(version = nil)
+      if version
+        super("Multiple migrations have the version number #{version}")
+      else
+        super("Duplicate migration version error")
+      end
     end
   end
 
   class DuplicateMigrationNameError < MigrationError#:nodoc:
-    def initialize(name)
-      super("Multiple migrations have the name #{name}")
+    def initialize(name = nil)
+      if name
+        super("Multiple migrations have the name #{name}")
+      else
+        super("Duplicate migration name")
+      end
     end
   end
 
   class UnknownMigrationVersionError < MigrationError #:nodoc:
-    def initialize(version)
-      super("No migration with version number #{version}")
+    def initialize(version = nil)
+      if version
+        super("No migration with version number #{version}")
+      else
+        super("Unknown migration version")
+      end
     end
   end
 
   class IllegalMigrationNameError < MigrationError#:nodoc:
-    def initialize(name)
-      super("Illegal name for migration file: #{name}\n\t(only lower case letters, numbers, and '_' allowed)")
+    def initialize(name = nil)
+      if name
+        super("Illegal name for migration file: #{name}\n\t(only lower case letters, numbers, and '_' allowed)")
+      else
+        super("Illegal name for migration")
+      end
     end
   end
 

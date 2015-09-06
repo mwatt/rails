b63956e140dcf8d67922d6c95e0dbd986c0a8cf4
Pavel Pravosud <pavel@pravosud.com>
2015-09-05 17:43:31 -0700

    Add test case for ActiveRecord::ActiveRecordError

---
+++ activerecord/test/cases/errors_test.rb
@@ -0,0 +1,17 @@
+require_relative "../cases/helper"
+require_relative "../../lib/active_record/errors"
+
+class ErrorsTest < ActiveRecord::TestCase
+  def test_can_be_instantiated_without_args
+    base = ActiveRecord::ActiveRecordError
+    error_klasses = ObjectSpace.each_object(Class).select { |klass| klass < base }
+
+    error_klasses.each do |error_klass|
+      begin
+        error_klass.new.inspect
+      rescue ArgumentError
+        raise "Instance of #{error_klass} can't be initialized with no arguments"
+      end
+    end
+  end
+end

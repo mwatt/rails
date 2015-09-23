f2314de3a05e4428c0a3c731026fdd941844a871
Ronak Jangir <ronakjangir47@gmail.com>
2015-09-24 00:16:42 +0530

    Assertion on datetime seconds precision is only valid for newer mysql versions

--- activerecord/test/cases/type/date_time_test.rb
+++ activerecord/test/cases/type/date_time_test.rb
@@ -5,8 +5,10 @@ module ActiveRecord
   module Type
     class IntegerTest < ActiveRecord::TestCase
       def test_datetime_seconds_precision_applied_to_timestamp
-        p = Task.create!(starting: ::Time.now)
-        assert_equal p.starting.usec, p.reload.starting.usec
+        if subsecond_precision_supported?
+          p = Task.create!(starting: ::Time.now)
+          assert_equal p.starting.usec, p.reload.starting.usec
+        end
       end
     end
   end

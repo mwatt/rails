mwatt/rails

commit 0a120a818d413c64ff9867125f0b03788fc306f8

Avoid to define an initializer after the load_config_initializers
This make the config/initializers run before the railties are loaded
what can break some configurations.

activesupport/lib/active_support.rb
@@ -73,6 +73,14 @@ module ActiveSupport
   end
 
   cattr_accessor :test_order # :nodoc:
+
+  def self.halt_callback_chains_on_return_false
+    Callbacks::CallbackChain.halt_and_display_warning_on_return_false
+  end
+
+  def self.halt_callback_chains_on_return_false=(value)
+    Callbacks::CallbackChain.halt_and_display_warning_on_return_false = value
+  end
 end
 
 autoload :I18n, "active_support/i18n"

railties/lib/rails/generators/rails/app/templates/config/initializers/callback_terminator.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Do not halt callback chains when a callback returns false.
-Rails.application.config.active_support.halt_callback_chains_on_return_false = false
+ActiveSupport.halt_callback_chains_on_return_false = false

activesupport/lib/active_support/railtie.rb
@@ -13,13 +13,6 @@ module ActiveSupport
       end
     end
 
-    initializer "active_support.halt_callback_chains_on_return_false", after: :load_config_initializers do |app|
-      if app.config.active_support.key? :halt_callback_chains_on_return_false
-        ActiveSupport::Callbacks::CallbackChain.halt_and_display_warning_on_return_false = \
-          app.config.active_support.halt_callback_chains_on_return_false
-      end
-    end
-
     # Sets the default value for Time.zone
     # If assigned value cannot be matched to a TimeZone, an exception will be raised.
     initializer "active_support.initialize_time_zone" do |app|

commit bcad566940777cec9983becb6d3740352072c333

Refactor AS::Callbacks halt config and fix the documentation
Move from `AS::Callbacks::CallbackChain.halt_and_display_warning_on_return_false`
to `AS::Callbacks.halt_and_display_warning_on_return_false` base on
[this
discussion](https://github.com/rails/rails/pull/21218#discussion_r39354580)

Fix the documentation broken by 0a120a818d413c64ff9867125f0b03788fc306f8

activesupport/lib/active_support.rb
@@ -75,11 +75,11 @@ module ActiveSupport
   cattr_accessor :test_order # :nodoc:
 
   def self.halt_callback_chains_on_return_false
-    Callbacks::CallbackChain.halt_and_display_warning_on_return_false
+    Callbacks.halt_and_display_warning_on_return_false
   end
 
   def self.halt_callback_chains_on_return_false=(value)
-    Callbacks::CallbackChain.halt_and_display_warning_on_return_false = value
+    Callbacks.halt_and_display_warning_on_return_false = value
   end
 end
 

activesupport/lib/active_support/callbacks.rb
@@ -66,6 +66,13 @@ module ActiveSupport
 
     CALLBACK_FILTER_TYPES = [:before, :after, :around]
 
+    # If +true+, Active Record and Active Model callbacks returning +false+ will
+    # halt the entire callback chain and display a deprecation message.
+    # If +false+, callback chains will only be halted by calling +throw :abort+.
+    # Defaults to +true+.
+    mattr_accessor :halt_and_display_warning_on_return_false
+    self.halt_and_display_warning_on_return_false = true
+
     # Runs the callbacks for the given event.
     #
     # Calls the before and around callbacks in the order they were set, yields
@@ -450,12 +457,6 @@ module ActiveSupport
 
       attr_reader :name, :config
 
-      # If true, any callback returning +false+ will halt the entire callback
-      # chain and display a deprecation message. If false, callback chains will
-      # only be halted by calling +throw :abort+. Defaults to +true+.
-      class_attribute :halt_and_display_warning_on_return_false
-      self.halt_and_display_warning_on_return_false = true
-
       def initialize(name, config)
         @name = name
         @config = {
@@ -760,7 +761,7 @@ module ActiveSupport
           terminate = true
           catch(:abort) do
             result = result_lambda.call if result_lambda.is_a?(Proc)
-            if CallbackChain.halt_and_display_warning_on_return_false && result == false
+            if Callbacks.halt_and_display_warning_on_return_false && result == false
               display_deprecation_warning_for_false_terminator
             else
               terminate = false

railties/lib/rails/generators/rails/app/templates/config/initializers/callback_terminator.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-# Do not halt callback chains when a callback returns false.
+# Do not halt callback chains when a callback in Active Record, Active Model
+# or Active Model validations returns false.
 ActiveSupport.halt_callback_chains_on_return_false = false

activesupport/test/callbacks_test.rb
@@ -776,7 +776,7 @@ module CallbacksTest
 
   class CallbackFalseTerminatorWithConfigTrueTest < ActiveSupport::TestCase
     def setup
-      ActiveSupport::Callbacks::CallbackChain.halt_and_display_warning_on_return_false = true
+      ActiveSupport::Callbacks.halt_and_display_warning_on_return_false = true
     end
 
     def test_returning_false_does_not_halt_callback_if_config_variable_is_true
@@ -789,7 +789,7 @@ module CallbacksTest
 
   class CallbackFalseTerminatorWithConfigFalseTest < ActiveSupport::TestCase
     def setup
-      ActiveSupport::Callbacks::CallbackChain.halt_and_display_warning_on_return_false = false
+      ActiveSupport::Callbacks.halt_and_display_warning_on_return_false = false
     end
 
     def test_returning_false_does_not_halt_callback_if_config_variable_is_false
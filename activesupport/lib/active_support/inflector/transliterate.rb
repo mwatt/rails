2f4081e7faaf08b34de7cba3e9a29e370fcae887
Swaathi K <swaathi@skcript.com>
2015-10-07 22:56:55 +0530

    Updated parameterize in transliterate.rb

--- activesupport/lib/active_support/inflector/transliterate.rb
+++ activesupport/lib/active_support/inflector/transliterate.rb
@@ -72,18 +72,18 @@ module ActiveSupport
       # replace accented chars with their ascii equivalents
       parameterized_string = transliterate(string)
       # Turn unwanted chars into the separator
-      parameterized_string.gsub!(/[^a-z0-9\-_]+/i, sep)
-      unless sep.nil? || sep.empty?
-        if sep == "-".freeze
+      parameterized_string.gsub!(/[^a-z0-9\-_]+/i, separator)
+      unless separator.nil? || separator.empty?
+        if separator == "-".freeze
           re_duplicate_separator        = /-{2,}/
           re_leading_trailing_separator = /^-|-$/i
         else
-          re_sep = Regexp.escape(sep)
+          re_sep = Regexp.escape(separator)
           re_duplicate_separator        = /#{re_sep}{2,}/
           re_leading_trailing_separator = /^#{re_sep}|#{re_sep}$/i
         end
         # No more than one of the separator in a row.
-        parameterized_string.gsub!(re_duplicate_separator, sep)
+        parameterized_string.gsub!(re_duplicate_separator, separator)
         # Remove leading/trailing separator.
         parameterized_string.gsub!(re_leading_trailing_separator, ''.freeze)
       end
